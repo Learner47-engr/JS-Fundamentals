983661530b507ad24efd0df0a6a77f75d2a3ac01
Mighty Ayanbamiji <mightytomisin@gmail.com>
2025-07-13 01:44:00 +0100

    updated the value argument file

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,14 +1,4 @@
-// const numberOfUserArguments = process.argv.length - 2;
 
-// if (numberOfUserArguments === 0) {
-//     // If no arguments are passed by the user
-//     console.log("No argument");
-// } else if (numberOfUserArguments === 1) {
-//     // If exactly one argument is passed by the user
-//     console.log("Argument found");
-// } else {
-//     // If more than one argument is passed by the user
-//     console.log("Arguments found");
 
 let count = 0;
 while (process.argv[count + 2] !== undefined) {
